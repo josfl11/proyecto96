5c4a503df54b9029187ed6ff55a0e09d54801ade
josfl11 <143562558+josfl11@users.noreply.github.com>
2023-12-12 18:19:39 -0600

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,16 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Establece "player1_name" usando la función localStorage.setItem()
+	  //Establece "player2_name" usando la función localStorage.setItem()
+function player1_name(){
+  localStorage.setItem()
+}
+function player2_name(){
+  localStorage.setItem()
+}
+    window.location = "game_page.html";
+}
+
